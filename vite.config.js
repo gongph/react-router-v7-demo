f8380733985669a41df9badcf4ab0ab89e62eab8
gongph <gongph@foxmail.com>
2024-12-05 17:11:28 +0800

    chore: add env

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 // import react from '@vitejs/plugin-react-swc'
 import { reactRouter } from "@react-router/dev/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "",
-  plugins: [reactRouter()],
+export default defineConfig(({ mode }) => {
+  // eslint-disable-next-line no-undef
+  const env = loadEnv(mode, process.cwd());
+  console.log("env :>> ", env);
+  return {
+    base: env.VITE_BASE,
+    plugins: [reactRouter()],
+  };
 });
