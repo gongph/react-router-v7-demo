3598c324068acd3c95a1384bf12cf180cf6cc3f3
gongph <gongph@foxmail.com>
2024-12-05 17:58:47 +0800

    chore: env del

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import { defineConfig, loadEnv } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  // eslint-disable-next-line no-undef
-  const env = loadEnv(mode, process.cwd());
-  return {
-    base: env.VITE_BASE,
-    plugins: [reactRouter()],
-  };
+export default defineConfig({
+  plugins: [reactRouter()],
 });
