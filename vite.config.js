82277ef2d7da671213ef2113d168b1ca724ed439
gongph <gongph@foxmail.com>
2024-12-05 17:21:11 +0800

    chore: update base url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import { reactRouter } from "@react-router/dev/vite";
 export default defineConfig(({ mode }) => {
   // eslint-disable-next-line no-undef
   const env = loadEnv(mode, process.cwd());
-  console.log("env :>> ", env);
   return {
     base: env.VITE_BASE,
     plugins: [reactRouter()],
